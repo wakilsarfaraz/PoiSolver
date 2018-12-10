9cd0aeb6aaf810d38fac11a02aedca0b3922e2d7
Wakil Sarfaraz <wakilsarfaraz@gmail.com>
2018-12-10 15:03:41 +0000

    Add files via upload

---
+++ PoiSolver.R
@@ -0,0 +1,102 @@
+# library(plotly)
+# library(ggplot2)
+# library(geometry)
+# library(geomorph)
+
+
+xmax = 1
+N = 50
+X = seq(0,xmax,len=N+1)
+m = length(X); n=length(X);
+x = matrix(rep(X,each=n),nrow=n);
+y = matrix(rep(X,m),nrow=n)
+x = c(x)
+y = c(y)
+NNODES = (N+1)^2
+NTRI = 2*N^2
+LNODES = matrix(0,NTRI,3)
+
+for (i in 1:N){
+	for (j in 1:N){
+		    LNODES[i+2*(j-1)*N,1] = i+(j-1)*(N+1)
+        LNODES[i+2*(j-1)*N,2] = i+j*(N+1)
+        LNODES[i+2*(j-1)*N,3] = (i+1)+(j-1)*(N+1)
+        LNODES[i+N+2*(j-1)*N,1] = i+1+j*(N+1)
+        LNODES[i+N+2*(j-1)*N,2] = (i+1)+(j-1)*(N+1)
+        LNODES[i+N+2*(j-1)*N,3] = i+j*(N+1)
+    }
+}
+
+SP <- matrix(0, NNODES, NNODES)
+LV = matrix(0, NNODES, 1)
+
+
+for (n in 1:NTRI)
+	{
+	r1 = matrix(c(x[LNODES[n,1]],y[LNODES[n,1]]),nrow=2, byrow=FALSE)
+  r2 = matrix(c(x[LNODES[n,2]],y[LNODES[n,2]]),nrow=2, byrow=FALSE);
+  r3 = matrix(c(x[LNODES[n,3]],y[LNODES[n,3]]),nrow=2, byrow=FALSE);
+  J = matrix(c(r2[1]-r1[1],r2[2]-r1[2]
+    ,r3[1]-r1[1],r3[2]-r1[2]), nrow=2, byrow=TRUE) 
+  Astiff = (1/(2*det(J)))*matrix(c(sum((r2-r3)*(r2-r3))
+          ,sum((r2-r3)*(r3-r1)),sum((r2-r3)*(r1-r2))
+          ,sum((r2-r3)*(r3-r1)),sum((r3-r1)*(r3-r1))
+          ,sum((r3-r1)*(r1-r2)),sum((r2-r3)*(r1-r2))
+          ,sum((r3-r1)*(r1-r2)),sum((r1-r2)*(r1-r2))), nrow=3, byrow=TRUE);
+  for (i in 1:3){
+      for (j in 1:3){
+        SP[LNODES[n,i],LNODES[n,j]]=SP[LNODES[n,i],LNODES[n,j]]+Astiff[i,j]
+      }
+    }
+  ksi = 1/3;
+  eta = 1/3;
+  xx = (1-ksi-eta)*r1[1]+ksi*r2[1]+eta*r3[1];
+  yy = (1-ksi-eta)*r1[2]+ksi*r2[2]+eta*r3[2];
+  
+  # F[1] = (1-ksi-eta)*det(J)*1/2;
+  # F[2] = (ksi)*det(J)*1/2;
+  # F[3] = (eta)*det(J)*1/2;
+  
+  F[1] = (1-ksi-eta)*5*pi^2*sin(pi*xx)*sin(2*pi*yy)*det(J)*1/2;
+  F[2] = (ksi)*5*pi^2*sin(pi*xx)*sin(2*pi*yy)*det(J)*1/2;
+  F[3] = (eta)*5*pi^2*sin(pi*xx)*sin(2*pi*yy)*det(J)*1/2;
+  
+  # F[1] = pi^2*((1-ksi-eta)*det(J)*(1/2))*(yy*sin(2*pi*xx)+xx*sin(2*pi*yy));
+  # F[2] = pi^2*((ksi)*det(J)*(1/2))*(yy*sin(2*pi*xx)+xx*sin(2*pi*yy));
+  # F[3] = pi^2*((eta)*det(J)*(1/2))*(yy*sin(2*pi*xx)+xx*sin(2*pi*yy));
+
+  
+  for (i in 1:3){
+    LV[LNODES[n,i]] = LV[LNODES[n,i]]+ F[i]
+    }
+	}
+for (i in 1:NNODES){
+if (x[i]==0 | y[i]==0 | x[i]==xmax | y[i]==xmax){
+    LV[i] = 0
+    SP[i,] = 0
+    SP[i,i] = 1
+     }
+}
+
+
+
+U = solve(SP,LV)
+
+
+
+
+
+
+
+
+
+
+
+
+
+write.table(x,file="/Users/sarfaraw/Dropbox/ICGS/PIQ/R-Tutorial/Laplace/coordx.txt",row.names=FALSE,col.names=FALSE)
+write.table(y,file="/Users/sarfaraw/Dropbox/ICGS/PIQ/R-Tutorial/Laplace/coordy.txt",row.names=FALSE,col.names=FALSE)
+write.table(U,file="/Users/sarfaraw/Dropbox/ICGS/PIQ/R-Tutorial/Laplace/numsol.txt",row.names=FALSE,col.names=FALSE)
+write.table(LNODES,file="/Users/sarfaraw/Dropbox/ICGS/PIQ/R-Tutorial/Laplace/triangles.txt",row.names=FALSE,col.names=FALSE)
+
+
