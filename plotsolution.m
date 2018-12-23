d7d8bf818ca2a455c1a9482ed5bee0f194bab809
Wakil <wakilsarfaraz@gmail.com>
2018-12-23 17:17:13 +0000

    Graphics

--- plotsolution.m
+++ plotsolution.m
@@ -5,7 +5,7 @@ triangles = importdata('triangles.txt');
 numsol = importdata('Solutions.txt');
 figure(1)
 trisurf(triangles,coordx,coordy,numsol)
-shading interp
+%shading interp
 title('Finite element solution','fontsize',20)
 xlabel('x','fontsize',16)
 ylabel('y','fontsize',16)
